6ad1250fa96355870c119a0fa79fe008c218145f
truonghungcuong <truonghungcuong12012001@gmail.com>
2022-02-15 10:35:06 +0700

    CaseStudy2

--- src/com/codegym/view/CustomerMenu.java
+++ src/com/codegym/view/CustomerMenu.java
@@ -35,61 +35,7 @@ public class CustomerMenu {
                     break;
                 }
                 case 2: {
-                    productManagement.displayAll();
-                    productManagement.clearCustomerProduct();
-                    productManagement.clearTotalPaymoney();
-                    int choicePrice = -1;
-                    do {
-                        menuPrice();
-                        System.out.println("Nhập lựa chọn");
-                        choicePrice = scanner.nextInt();
-                        if (choicePrice == 1) {
-                            System.out.println("Nhập mã sản phẩm muốn mua");
-                            scanner.nextLine();
-                            String id = scanner.nextLine();
-                            System.out.println("Nhập số lượng muốn mua");
-                            int quanlity = scanner.nextInt();
-                            int index = productManagement.findProductById(id);
-
-                            if (index == -1) {
-                                System.out.println("Mã sản phẩm không đúng");
-                            } else {
-                                if (quanlity > productManagement.getProducts().get(index).getQuanlity()) {
-                                    System.out.println("Không đủ sản phẩm");
-                                } else {
-                                    productManagement.getProductSold().put(productManagement.getProducts().get(index), quanlity);
-                                    productManagement.getCustomerProduct().put(productManagement.getProducts().get(index), quanlity);
-                                    productManagement.displayByProductCustomer();
-                                    productManagement.PaymentProduct(index, quanlity);
-                                    System.out.println("Tổng số tiền phải trả: " + productManagement.totalPaymoney());
-                                    productManagement.getProducts().get(index).setQuanlity(productManagement.getProducts().get(index).getQuanlity() - quanlity);
-                                    try {
-                                        productManagement.WriteFileProductSold("productsold.txt");
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
-                                    try {
-                                        productManagement.WriteFileLeader("productleader.txt");
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
-                                    try {
-                                        productManagement.WriteFileCustomer("productcustomer.txt");
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
-                                    try {
-                                        productManagement.WriteFileTotalPaymoney("totalpaymoney.txt");
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
-
-
-                                }
-                            }
-                        }
-                    } while (choicePrice != 0);
-
+                    showPurchasedProduct(productManagement);
                     break;
                 }
                 case 3: {
@@ -131,6 +77,63 @@ public class CustomerMenu {
             System.out.println("---Danh sách sản phẩm---");
             productManagement.displayAll();
         }
+    }
+
+    private void showPurchasedProduct(ProductManagement productManagement) {
+        productManagement.displayAll();
+        productManagement.clearCustomerProduct();
+        productManagement.clearTotalPaymoney();
+        int choicePrice = -1;
+        do {
+            menuPrice();
+            System.out.println("Nhập lựa chọn");
+            choicePrice = scanner.nextInt();
+            if (choicePrice == 1) {
+                System.out.println("Nhập mã sản phẩm muốn mua");
+                scanner.nextLine();
+                String id = scanner.nextLine();
+                System.out.println("Nhập số lượng muốn mua");
+                int quanlity = scanner.nextInt();
+                int index = productManagement.findProductById(id);
+
+                if (index == -1) {
+                    System.out.println("Mã sản phẩm không đúng");
+                } else {
+                    if (quanlity > productManagement.getProducts().get(index).getQuanlity()) {
+                        System.out.println("Không đủ sản phẩm");
+                    } else {
+                        productManagement.getProductSold().put(productManagement.getProducts().get(index), quanlity);
+                        productManagement.getCustomerProduct().put(productManagement.getProducts().get(index), quanlity);
+                        productManagement.displayByProductCustomer();
+                        productManagement.PaymentProduct(index, quanlity);
+                        System.out.println("Tổng số tiền phải trả: " + productManagement.totalPaymoney());
+                        productManagement.getProducts().get(index).setQuanlity(productManagement.getProducts().get(index).getQuanlity() - quanlity);
+                        try {
+                            productManagement.WriteFileProductSold("productsold.txt");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            productManagement.WriteFileLeader("productleader.txt");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            productManagement.WriteFileCustomer("productcustomer.txt");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            productManagement.WriteFileTotalPaymoney("totalpaymoney.txt");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }
+            }
+        } while (choicePrice != 0);
     }
 
     private void menuPrice() {
